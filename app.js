710c2051b69ec821ca29f28e570e61b059549cb1
Dennis Gauss <chan05@hotmail.de>
2022-02-24 10:30:34 +0100

    Added list room functionality

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var roomRouter = require('./routes/rooms');
 
+// create database
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+// create database tables
+db.run('CREATE TABLE room(Id integer, ownerId integer , PRIMARY KEY (Id))');
+
+
 var app = express();
 
 // view engine setup
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,14 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var roomList = [];
 
 /* GET rooms */
 router.get('/', function(req, res, next) {
-  res.send('list of rooms');
+
+  db.all(`SELECT Id from rooms`, [], (err, rows) => 
+   {
+     if (err) { throw(err); }
+     rows.array.forEach(row => {
+       roomList.push( row );
+     });
+   });
+
+  res.send(JSON.stringify(roomList));
 });
 
 /* GET single room */
 router.get('/:roomId', function(req, res, next) {
   res.send(req.params.roomId);
 });
+
+router.post('/', function (req, res) {
+  var body = JSON.parse(req.body.text);
+  var roomId = Math.random();
+
+  // insert room into table
+  db.run(`INSERT INTO rooms(Id) VALUES(?,?)`, [roomId, 0], function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with rowid ${this.lastID}`);
+  });
+
+  // close the database connection
+  db.close();
+
+})
 
 module.exports = router;
